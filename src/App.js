fca24f357e705c0ac8ee47e84389dafd6bc5e6ec
Docta Rhyf <drrhyf@gmail.com>
2023-12-25 08:19:09 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" h-[100vh] ">
+      <section className="bg-sky-500 h-full w-44">
+        <div className="text-white  border-b pb-4 text-3xl p-2 text-center">
+          工人管理
+        </div>
+        <div>
+          <ul className="text-end p-2">
+            {[...Array(5)].map((it, i) => (
+              <li className="mb-2" key={i}>
+                <button className="text-right cursor-pointer hover:text-sky-500 hover:bg-white w-full  rounded-md p-2 ">
+                  Item
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </section>
     </div>
   );
 }
---
+++ src/helpers/flow.js
@@ -0,0 +1,5 @@
+const MAIN_MENU = [
+  { name: "Agents", path: "/agents" },
+  { name: "Roulements", path: "/roulements" },
+  { name: "Equipes", path: "/equipes" },
+];
