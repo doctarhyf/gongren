cdad9048f45ad07ba4a4905d5fc07916c8bfe4e2
Docta Rhyf <drrhyf@gmail.com>
2023-12-26 12:26:25 +0200

    agents filters

--- src/helpers/flow.js
+++ src/helpers/flow.js
@@ -7,10 +7,14 @@ import Sections from "../pages/sections";
 export const CLASS_BTN =
   " mx-1 hover:bg-sky-500 hover:text-white border p-1 text-sky-400 border-transparent cursor-pointer rounded-md";
 
+export const SECTIONS = ["BROYAGE", "ENSACHAGE"];
+export const POSTE = ["NET", "EXP", "OPE", "CHARG", "MEC"];
+export const EQUIPES = ["JR", "A", "B", "C", "D"];
+
 export const MAIN_MENU = [
   { name: "Home", path: "/home", el: Home },
   { name: "Agents", path: "/agents", el: Agents },
   { name: "Roulements", path: "/roulements", el: Roulements },
   { name: "Equipes", path: "/equipes", el: Equipes },
-  { name: "Sections", path: "/sections", el: Sections },
+  /*{ name: "Sections", path: "/sections", el: Sections }, */
 ];
--- src/pages/equipes.js
+++ src/pages/equipes.js
@@ -1,5 +1,89 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { EQUIPES, POSTE, SECTIONS } from "../helpers/flow";
+import { LoadAllItems } from "../helpers/sb";
+import { TABLES_NAMES } from "../helpers/sb.config";
 
 export default function Equipes() {
-  return <div>Equipes</div>;
+  const [agents, setagents] = useState([]);
+  const [agentsf, setagentsf] = useState([]);
+
+  const ref_equipe = useRef();
+  const ref_section = useRef();
+
+  useEffect(() => {
+    loadAgents();
+  }, []);
+
+  async function loadAgents() {
+    let items = await LoadAllItems(TABLES_NAMES.AGENTS);
+
+    setagents(items);
+  }
+
+  function FilterAgents(items_raw, section, equipe) {
+    let items = items_raw.filter((ag, i) => {
+      const check_equipe = ag.equipe === equipe;
+      const check_section = ag.section === section;
+
+      return check_equipe && check_section;
+    });
+
+    return items;
+  }
+
+  function onChange(e) {
+    const section = ref_section.current.value;
+    const equipe = ref_equipe.current.value;
+
+    const itemsf = FilterAgents(agents, section, equipe);
+
+    setagentsf(itemsf);
+  }
+  return (
+    <div>
+      <table>
+        <tbody>
+          <tr>
+            <td>SECTION</td>
+            <td>
+              <select
+                name="section"
+                ref={ref_section}
+                defaultValue={SECTIONS[0]}
+                onChange={onChange}
+              >
+                {SECTIONS.map((it, i) => (
+                  <option key={i}>{it}</option>
+                ))}
+              </select>
+            </td>
+          </tr>
+          <tr>
+            <td>EQUIPE</td>
+            <td>
+              {" "}
+              <select
+                name="equipe"
+                defaultValue={EQUIPES[0]}
+                ref={ref_equipe}
+                onChange={onChange}
+              >
+                {EQUIPES.map((it, i) => (
+                  <option key={i}>{it}</option>
+                ))}
+              </select>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      <div className="text-3xl">Equipe ... - ...</div>
+
+      <div>
+        {agentsf.map((it, i) => (
+          <div key={i}>{it.nom}</div>
+        ))}
+      </div>
+    </div>
+  );
 }
