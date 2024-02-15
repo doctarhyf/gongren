d5ffa81efbc51bc0e322f7005d58f6e3c55e78e4
Docta Rhyf <drrhyf@gmail.com>
2024-02-15 14:43:07 +0200

    dbg

--- src/comps/FormNewWord.js
+++ src/comps/FormNewWord.js
@@ -11,8 +11,13 @@ import Loading from "../comps/Loading";
 
 function PYKBD({ show, onType, onHidePYKBD }) {
   return (
-    <div className={` ${show ? "block" : "hidden"} `}>
-      <div>
+    <div
+      className={` border  p-1 px-2 rounded-md bg-neutral-200/70 shadow-md ${
+        show ? "block" : "hidden"
+      } `}
+    >
+      <div className="flex flex-row font-bold text-xs items-center my-2  justify-between">
+        <span>PINYIN KEYBOARD</span>
         <button
           onClick={onHidePYKBD}
           className="text-white bg-red-500 text-xs rounded-full w-6 h-6"
@@ -23,7 +28,7 @@ function PYKBD({ show, onType, onHidePYKBD }) {
 
       <div>
         {pinyinVowelsWithTones.map((pyletter_array, i) => (
-          <div key={i}>
+          <div key={i} className="flex justify-between">
             {pyletter_array.map((py, i) => (
               <button key={i} className={CLASS_BTN} onClick={(e) => onType(py)}>
                 {py}
--- src/comps/ImageUpload.js
+++ src/comps/ImageUpload.js
@@ -12,7 +12,7 @@ export function compressImage(file) {
       const canvas = document.createElement("canvas");
       const context = canvas.getContext("2d");
 
-      const targetWidth = 180;
+      const targetWidth = 280;
       const targetHeight = (image.height / image.width) * targetWidth;
 
       canvas.width = targetWidth;
