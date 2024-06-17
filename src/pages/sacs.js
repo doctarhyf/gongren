e9a2eaa11a721d55968be7b97253cedf9cb19f15
Docta Rhyf <drrhyf@gmail.com>
2024-06-17 15:35:45 +0200

    working on insertion

--- src/pages/sacs.js
+++ src/pages/sacs.js
@@ -24,8 +24,89 @@ const SECTIONS = {
   CALCULATOR: { label: "Sacs Calculator" },
 };
 
-function SacsCont({ trans }) {
-  return <div></div>;
+function SacsCont({ trans, onAddTrans }) {
+  const [showInput, setShowInput] = useState(false);
+
+  function onSaveTrans() {
+    setShowInput(false);
+    onAddTrans("cont", { team: "A", s32: Math.random(), s42: Math.random() });
+  }
+
+  return (
+    <div>
+      <div>
+        {!showInput && (
+          <button
+            onClick={(e) => setShowInput(true)}
+            className=" p-1 text-green-500 border rounded-md border-green-500 hover:text-white hover:bg-green-500 "
+          >
+            INSERT
+          </button>
+        )}
+
+        {showInput && (
+          <button
+            onClick={onSaveTrans}
+            className=" p-1 text-sky-500 border rounded-md border-sky-500 hover:text-white hover:bg-sky-500 "
+          >
+            SAVE
+          </button>
+        )}
+
+        <button
+          onClick={(e) => setShowInput(false)}
+          className=" p-1 text-red-500 border rounded-md border-red-500 hover:text-white hover:bg-red-500 "
+        >
+          CANCEL
+        </button>
+      </div>
+      <div className=" container  ">
+        <table>
+          <thead>
+            <th className="p1 border border-gray-900">id</th>
+            <th className="p1 border border-gray-900">Equipe</th>
+            <th className="p1 border border-gray-900">32.5</th>
+            <th className="p1 border border-gray-900">42.5</th>
+            <th className="p1 border border-gray-900">Date</th>
+          </thead>
+          <tbody>
+            {showInput && (
+              <tr>
+                <td className="p1 border border-gray-900">{-1}</td>
+                <td className="p1 border border-gray-900">
+                  <select className=" border p-1 ">
+                    {["A", "B", "C", "D"].map((eq) => (
+                      <option value={eq}>{eq}</option>
+                    ))}
+                  </select>
+                </td>
+                <td className="p1 border border-gray-900">
+                  <input className=" w-16 " type="number" value={0} />
+                </td>
+                <td className="p1 border border-gray-900">
+                  <input className=" w-16" type="number" value={0} />
+                </td>
+                <td className="p1 border border-gray-900">
+                  {new Date().toDateString()}
+                </td>
+              </tr>
+            )}
+            {trans.map((t, i) => (
+              <tr className={`  ${showInput ? "opacity-20" : ""}  `}>
+                <td className="p1 border border-gray-900">{i}</td>
+                <td className="p1 border border-gray-900">{t.team}</td>
+                <td className="p1 border border-gray-900">{t.s32}</td>
+                <td className="p1 border border-gray-900">{t.s42}</td>
+                <td className="p1 border border-gray-900">
+                  {new Date().toDateString()}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default function Sacs() {
@@ -38,6 +119,13 @@ export default function Sacs() {
     setcurtab(t);
   }
 
+  function onAddTrans(type, data) {
+    set_trans_cont((old) => [...old, data]);
+
+    console.log(type, data);
+    console.log(trans_cont);
+  }
+
   return (
     <div>
       <TabCont tabs={SECTIONS} onSelectTab={onSelectTab} />
@@ -47,7 +135,7 @@ export default function Sacs() {
             <div>Production</div>
           )}
           {SECTIONS.CONTAINER.label === curtab[1].label && (
-            <SacsCont trans={trans_cont} />
+            <SacsCont trans={trans_cont} onAddTrans={onAddTrans} />
           )}
           {SECTIONS.CALCULATOR.label === curtab[1].label && (
             <div>Calculator</div>
