5020c354a9f7e53cf91d9ad10b420879e20f4ed8
Docta Rhyf <drrhyf@gmail.com>
2024-06-26 11:20:28 +0200

    added production data

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,7 +2,7 @@ import React, { Children, useEffect, useState } from "react";
 import * as SB from "../helpers/sb";
 import { TABLES_NAMES } from "../helpers/sb.config";
 import Loading from "../comps/Loading";
-import { GroupBySectionAndEquipe } from "../helpers/func";
+import { formatAsMoney, GroupBySectionAndEquipe } from "../helpers/func";
 import { LANG_COOKIE_KEY } from "../helpers/flow";
 import { useCookies } from "react-cookie";
 import {
@@ -31,6 +31,98 @@ const Card = ({ id, title, desc, children }) => {
   );
 };
 
+function Tonnage() {
+  const date = new Date();
+  const m = date.getMonth();
+  const y = date.getFullYear();
+  const d = date.getDate();
+  const [loading, setloading] = useState(false);
+
+  const [loads, setloads] = useState([]);
+  const [data, setdata] = useState({
+    camions: 100,
+    sacs: 200,
+    tonnage: 300,
+  });
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  function loadData() {
+    setloading(true);
+    SB.LoadAllItems2(
+      TABLES_NAMES.LOADS,
+      (s) => {
+        setloading(false);
+        let mstring = m + 1 < 10 ? "0" + (m + 1) : "" + (m + 1);
+        const monthCheck = `${y}-${mstring}-`;
+
+        const curMonthLoads = s.filter(
+          (it, i) => it.created_at.split(":")[0].split(`-${mstring}-`)[0] == y
+        );
+        setloads(curMonthLoads);
+
+        let totCamions = 0;
+        let totSacs = 0;
+        let totTonnage = 0;
+
+        curMonthLoads.forEach((el) => {
+          const {
+            ajouts,
+            autre,
+            camions,
+            code,
+            created_at,
+            dechires,
+            id,
+            prob_courant,
+            prob_machine,
+            retours,
+            sacs,
+          } = el;
+
+          totSacs += parseInt(sacs);
+          totTonnage = totSacs / 20;
+          totCamions += parseInt(camions);
+        });
+
+        setdata({
+          camions: totCamions,
+          sacs: totSacs,
+          tonnage: `${totTonnage} T`,
+        });
+      },
+      (e) => {
+        setloading(false);
+        console.log(e);
+        alert(`Error \n ${JSON.stringify(e)}`);
+      }
+    );
+  }
+
+  return (
+    <Card id={3} title={`PROD./生产 年${y}月${m}日${d} `} desc={""}>
+      {loading ? (
+        <Loading isLoading={true} />
+      ) : (
+        <div className="">
+          {[
+            ["CAMIONS/车数", data.camions],
+            ["SACS/袋数", data.sacs],
+            ["TON./吨数", data.tonnage],
+          ].map((it, i) => (
+            <div>
+              <div className=" text-xs  ">{it[0]}</div>
+              <div className=" text-[24pt] ">{it[1]}</div>
+            </div>
+          ))}
+        </div>
+      )}
+    </Card>
+  );
+}
+
 export default function Home() {
   const [agents, setagents] = useState([]);
   const [loading, setloading] = useState(false);
@@ -46,7 +138,7 @@ export default function Home() {
   const [lang, setlang] = useState(LANGS[1]);
 
   useEffect(() => {
-    loadData();
+    // loadData();
     const sellang = LANGS[cookies[LANG_COOKIE_KEY]] || LANGS[1];
     setlang(sellang);
     settrads(GEN_TRANSLATIONS(TRANSLATIONS, sellang));
@@ -91,13 +183,7 @@ export default function Home() {
       <Loading isLoading={loading} />
 
       <div className=" container flex gap-4 my-4 flex-col md:flex-row ">
-        {[...Array(5).fill(0)].map((it, idx) => (
-          <Card
-            id={idx}
-            title={"PRODUCTION"}
-            desc={"Ceci montre la production"}
-          ></Card>
-        ))}
+        <Tonnage />
       </div>
 
       {false && (
