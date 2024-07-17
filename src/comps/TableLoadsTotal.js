4c5331bcab7994845b7bf4982d338c33ac3146ce
Docta Rhyf <drrhyf@gmail.com>
2024-07-17 23:25:10 +0200

    only show bonus for current team

--- src/comps/TableLoadsTotal.js
+++ src/comps/TableLoadsTotal.js
@@ -1,6 +1,12 @@
-import React, { useEffect, useState } from "react";
-import { CLASS_TD, COLUMNS_TO_HIDE } from "../helpers/flow";
-import { formatAsMoney, AddLeadingZero, ParseDate } from "../helpers/func";
+import React, { useContext, useEffect, useState } from "react";
+import { ACCESS_CODES, CLASS_TD, COLUMNS_TO_HIDE } from "../helpers/flow";
+import {
+  formatAsMoney,
+  AddLeadingZero,
+  ParseDate,
+  UserHasAccessCode,
+} from "../helpers/func";
+import { UserContext } from "../App";
 
 export default function TableLoadsTotals({
   totalData,
@@ -9,6 +15,7 @@ export default function TableLoadsTotals({
   lastUpdateDate,
   tableMode = true,
 }) {
+  const [, , user, setuser] = useContext(UserContext);
   const [upddate, setupddate] = useState("");
   const no_data = totalData.length === 0;
 
@@ -108,29 +115,33 @@ export default function TableLoadsTotals({
       ) : (
         <div>
           {Object.entries(totalData).map((td, i) => (
-            <div className="  py-1 border-b border-white/15">
-              <div className=" flex justify-between ">
-                <div className=" font-bold  ">{td[0]}</div>
-                <div className=" text-end   ">
-                  <div className=" text-[12pt]  text-xs p-1 rounded-md   ">
-                    <span className="  font-bold ">
-                      {td[1].sacs} SACS / {(td[1].sacs / 20).toFixed(2)} T
-                    </span>
-                    <span className="  opacity-50  "> - CHARG.</span>
-                  </div>
-                  <div className=" text-[14pt]  text-xs p-1 rounded-md  ">
-                    {/* <span className=" font-black bg-emerald-950 p-1 mx-1 rounded-md  ">
+            <div className="  py-1 ">
+              {(td[0] === user.equipe ||
+                UserHasAccessCode(user, ACCESS_CODES.ROOT) ||
+                user.poste === "INT") && (
+                <div className=" border-b border-white/15 ">
+                  <div className=" flex justify-between ">
+                    <div className=" font-bold  ">{td[0]}</div>
+                    <div className=" text-end   ">
+                      <div className=" text-[12pt]  text-xs p-1 rounded-md   ">
+                        <span className="  font-bold ">
+                          {td[1].sacs} SACS / {(td[1].sacs / 20).toFixed(2)} T
+                        </span>
+                        <span className="  opacity-50  "> - CHARG.</span>
+                      </div>
+                      <div className=" text-[14pt]  text-xs p-1 rounded-md  ">
+                        {/* <span className=" font-black bg-emerald-950 p-1 mx-1 rounded-md  ">
                       {td[1].bonus.toFixed(2)} T
                     </span>{" "} */}
-                    <span className=" font-black bg-emerald-950 p-1 mx-1 rounded-md  ">
-                      {formatAsMoney((td[1].bonus * 1000).toFixed(2))}
-                    </span>{" "}
-                    <span className="  opacity-50 "> - BONUS</span>
+                        <span className=" font-black bg-emerald-950 p-1 mx-1 rounded-md  ">
+                          {formatAsMoney((td[1].bonus * 1000).toFixed(2))}
+                        </span>{" "}
+                        <span className="  opacity-50 "> - BONUS</span>
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </div>
-              <div>
-                {/* {i !== 4 && (
+                  <div>
+                    {/* {i !== 4 && (
                   <progress
                     className=" progress progress-warning  w-full  "
                     max={totalData.TOTAL.sacs}
@@ -139,17 +150,19 @@ export default function TableLoadsTotals({
                     }
                   />
                 )} */}
-                {i !== 4 && (
-                  <progress
-                    className=" progress progress-success w-full  "
-                    max={totalData.TOTAL.bonus}
-                    value={
-                      (td[1].bonus / totalData.TOTAL.bonus) *
-                      totalData.TOTAL.bonus
-                    }
-                  />
-                )}{" "}
-              </div>
+                    {i !== 4 && (
+                      <progress
+                        className=" progress progress-success w-full  "
+                        max={totalData.TOTAL.bonus}
+                        value={
+                          (td[1].bonus / totalData.TOTAL.bonus) *
+                          totalData.TOTAL.bonus
+                        }
+                      />
+                    )}{" "}
+                  </div>{" "}
+                </div>
+              )}
             </div>
           ))}
           <div>
