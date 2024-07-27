e7057eb04a5969e7d8a1aae85233d10172e4b764
Docta Rhyf <drrhyf@gmail.com>
2024-07-27 13:53:35 +0400

    gui fix

--- src/comps/bigbag/ListBigbagTrucks.js
+++ src/comps/bigbag/ListBigbagTrucks.js
@@ -3,6 +3,7 @@ import DateSelector from "../DateSelector";
 import { ParseDate } from "../../helpers/func";
 import * as SB from "../../helpers/sb";
 import { TABLES_NAMES } from "../../helpers/sb.config";
+import { CLASS_TD } from "../../helpers/flow";
 
 export default function ListBigbagTrucks() {
   const [dt, setdt] = useState(ParseDate(new Date(), false));
@@ -35,41 +36,41 @@ export default function ListBigbagTrucks() {
       <div>
         <table>
           <tr>
-            <td>ID</td>
-            <td>Equipe</td>
-            <td>Plaque</td>
-            <td>T</td>
-            <td>Bags</td>
-            <td>Date/Time</td>
-            <td>Bon</td>
-            <td>front</td>
-            <td>side</td>
+            <td className={CLASS_TD}>ID</td>
+            <td className={CLASS_TD}>Equipe</td>
+            <td className={CLASS_TD}>Plaque</td>
+            <td className={CLASS_TD}>T</td>
+            <td className={CLASS_TD}>Bags</td>
+            <td className={CLASS_TD}>Date/Time</td>
+            <td className={CLASS_TD}>Bon</td>
+            <td className={CLASS_TD}>front</td>
+            <td className={CLASS_TD}>side</td>
           </tr>
           {ls.map((l, i) => (
             <tr>
-              <td>{l.id}</td>
-              <td>{l.equipe}</td>
-              <td>{l.plaque}</td>
-              <td>{l.t}</td>
-              <td>{l.bags}</td>
-              <td>
+              <td className={CLASS_TD}>{l.id}</td>
+              <td className={CLASS_TD}>{l.equipe}</td>
+              <td className={CLASS_TD}>{l.plaque}</td>
+              <td className={CLASS_TD}>{l.t}</td>
+              <td className={CLASS_TD}>{l.bags}</td>
+              <td className={CLASS_TD}>
                 {l.date} a {l.time}
               </td>
-              <td>
+              <td className={CLASS_TD}>
                 <div className=" bg-slate-700 w-32 h-16 rounded-md overflow-hidden  ">
                   <a href={l.photos[0]}>
                     <img src={l.photos[0]} className=" cursor-pointer  " />
                   </a>
                 </div>
               </td>
-              <td>
+              <td className={CLASS_TD}>
                 <div className=" bg-slate-700 w-32 h-16 rounded-md overflow-hidden  ">
                   <a href={l.photos[0]}>
                     <img src={l.photos[1]} className=" cursor-pointer  " />
                   </a>
                 </div>
               </td>
-              <td>
+              <td className={CLASS_TD}>
                 <div className=" bg-slate-700 w-32 h-16 rounded-md overflow-hidden  ">
                   <a href={l.photos[0]}>
                     <img src={l.photos[2]} className=" cursor-pointer  " />
