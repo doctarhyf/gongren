8a22a6f363cdcf87c54bc8fe22f20b32d5845358
Docta Rhyf <drrhyf@gmail.com>
2024-09-08 11:35:15 +0200

    fix gui

--- src/comps/MainNav.js
+++ src/comps/MainNav.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { MAIN_MENU, USER_LEVEL } from "../helpers/flow";
 import gck from "../img/gck.png";
+import menu from "../img/menu.svg";
 import { UserHasAccessCode } from "../helpers/func";
 
 function MainNav({ user, onMenuClick, curPage, onLogout }) {
@@ -8,10 +9,10 @@ function MainNav({ user, onMenuClick, curPage, onLogout }) {
   const [hidden, sethidden] = useState(true);
 
   return (
-    <section className="relative bg-sky-500 h-min md:h-fit w-full  md:flex justify-between">
+    <section className=" p-1  relative flex items-start  bg-sky-500 h-min md:h-fit w-full  md:flex justify-between">
       <div
         onClick={(e) => sethidden(!hidden)}
-        className="p-1 cursor-pointer mx-auto w-fit max-w-[120pt] flex justify-center items-center "
+        className="p-1 cursor-pointer  w-fit max-w-[120pt] flex justify-center items-center "
       >
         <img src={gck} height={10} />
       </div>
@@ -34,6 +35,7 @@ function MainNav({ user, onMenuClick, curPage, onLogout }) {
                     className={` p-2
                 ${curPage === menu_item.path ? "text-sky-500 bg-white" : ""}
 
+               dark:text-white
                 text-center cursor-pointer
                  hover:text-sky-500 
                  hover:bg-white 
@@ -55,6 +57,28 @@ function MainNav({ user, onMenuClick, curPage, onLogout }) {
             </li>
           </ul>
         </div>
+      </div>
+
+      <div
+        className={` cursor-pointer ${hidden ? "my-auto  " : "  "}   `}
+        onClick={(e) => sethidden(!hidden)}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="feather feather-menu  "
+        >
+          <line x1="3" y1="12" x2="21" y2="12"></line>
+          <line x1="3" y1="6" x2="21" y2="6"></line>
+          <line x1="3" y1="18" x2="21" y2="18"></line>
+        </svg>
       </div>
     </section>
   );
