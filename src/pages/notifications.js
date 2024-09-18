67e804c48b895e49b4ea991cd317c7e11afca988
Docta Rhyf <drrhyf@gmail.com>
2024-09-18 14:20:11 +0200

    dbg

--- src/pages/notifications.js
+++ src/pages/notifications.js
@@ -60,14 +60,14 @@ const NOTS = [
   },
 ];
 
-const fetchPosts = async () => {
+/* const fetchPosts = async () => {
   const { data } = await axios.get(
     "https://jsonplaceholder.typicode.com/posts"
   );
   return data;
-};
+}; */
 
-const fetchnot = async () => {
+const fetchNotifications = async () => {
   const { data, error } = await supabase
     .from(TABLES_NAMES.NOTIFICATIONS)
     .select("*");
@@ -77,26 +77,105 @@ const fetchnot = async () => {
 };
 
 export default function Notifications() {
+  const [show, , user, setuser] = useContext(UserContext);
+  const [selectednot, setselectednot] = useState(undefined);
+  const [adding, setadding] = useState(false);
+
   const { data, error, isLoading, isError } = useQuery({
     queryKey: ["posts"],
-    queryFn: fetchnot,
+    queryFn: fetchNotifications,
   });
 
-  if (isLoading) return <div>Loading...</div>;
-  if (isError) return <div>This is a fucking error Error: {error.message}</div>;
+  console.log(fetchNotifications);
+
+  // if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error: {error.message}</div>;
 
   return (
-    <div>
-      <h2>Posts List</h2>
-      <ul>
-        {data.map((post) => (
-          <li key={post.id}>
-            {/* <strong>{post.title}</strong>
-            <p>{post.body}</p> */}
-            {Object.keys(post)}
-          </li>
+    <div className=" container  ">
+      <div>
+        NOTIFICATIONS{" "}
+        <span className=" bg-red-600 text-white text-sm rounded-full font-bold px-2 p-1  ">
+          En cours developement!!!
+        </span>
+        <span>
+          <Loading isLoading={isLoading} />
+        </span>
+      </div>
+      {UserHasAccessCode(user, ACCESS_CODES.CAN_POST_NOTIFICATIONS) ||
+        (true && (
+          <ActionButton
+            title={"Nouvelle Notification"}
+            icon={plus}
+            onClick={(e) => setadding(true)}
+          />
         ))}
-      </ul>
+      {adding ? (
+        <div>
+          Adding
+          <ActionButton title={"ok"} onClick={(e) => setadding(false)} />
+        </div>
+      ) : (
+        <>
+          {selectednot ? (
+            <div>
+              <h5>{selectednot.title}</h5>
+              <div>{selectednot.desc}</div>
+              <div className=" w-56 h-56 overflow-hidden rounded-md ">
+                <img src={truck} className=" hover:cursor-pointer  " />
+              </div>
+              <ActionButton
+                title={"OK"}
+                icon={check}
+                onClick={(e) => setselectednot(undefined)}
+              />
+            </div>
+          ) : (
+            <div>
+              <table class="table-fixed">
+                <thead>
+                  <tr>
+                    <th className=" p-1 border-slate-500 border ">ID</th>
+                    <th className=" p-1 border-slate-500 border ">Date</th>
+                    <th className=" p-1 border-slate-500 border ">Plaque</th>
+                    <th className=" p-1 border-slate-500 border ">Problem</th>
+                    <th className=" p-1 border-slate-500 border ">Audio</th>
+                    <th className=" p-1 border-slate-500 border ">Photo</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {data &&
+                    data.map((not, i) => (
+                      <tr
+                        onClick={(e) => setselectednot(not)}
+                        className="  hover:bg-slate-500 hover:text-white hover:cursor-pointer  "
+                      >
+                        <td className=" p-1 border-slate-500 border ">
+                          {i + 1}
+                        </td>
+                        <td className=" p-1 border-slate-500 border ">
+                          {not.date}
+                        </td>
+                        <td className=" p-1 border-slate-500 border ">
+                          {not.plaque}
+                        </td>
+                        <td className=" p-1 border-slate-500 border ">
+                          {not.problem}
+                        </td>
+                        <td className=" p-1 border-slate-500 border ">
+                          {not.audio}
+                        </td>
+                        <td className=" p-1 border-slate-500 border ">
+                          {not.images && not.images[0]}
+                        </td>
+                      </tr>
+                    ))}
+                </tbody>
+              </table>
+            </div>
+          )}
+        </>
+      )}
     </div>
   );
 }
